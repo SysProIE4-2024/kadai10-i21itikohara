686e392a786b313b0028cd9447c1bc05f4805876
i21itikohara <i21itikohara@tokuyama.ac.jp>
2024-07-09 15:25:50 +0900

    提出1回目

--- mysystem.c
+++ mysystem.c
@@ -9,16 +9,79 @@
 #include "mysystem.h"  // インタフェース
 
 // system関数のクローン
+static char *execpath="/bin/sh";
 int mysystem(char *command) {
-  int status = 0;
+  int status=0;
+  pid_t pid;
+  if(command == NULL) {
+    exit(1);
+  } else if((pid=fork())<0) {
+    exit(-1);
+  }
+  if(pid != 0) {
+    
+    while(wait(&status) != pid)
+      ;
+    
+  } else {
+    execl(execpath, "sh", "-c", command, (char *)NULL);
+    exit(127);
+  }
 
-  // ここにプログラムを書く
-
+  //return 0;
   return status;
 }
 
-/* 実行例
 
-ここに実行例を書く
+/* 実行例
+ichikoharario@ichikohararionoMacBook-Air kadai10-i21itikohara % make          
+cc -Wall -std=c99 -o mysysmain mysysmain.c  mysystem.c
+ichikoharario@ichikohararionoMacBook-Air kadai10-i21itikohara % ./mysysmain ls
+mysystem:
+Makefile	README.pdf	mysysmain.c	mysystem.h
+README.md	mysysmain	mysystem.c	x.txt
+retval = 00000000
+system:
+Makefile	README.pdf	mysysmain.c	mysystem.h
+README.md	mysysmain	mysystem.c	x.txt
+retval = 00000000
+
+　〜2つ以上のコマンドを入力したいとき〜
+ichikoharario@ichikohararionoMacBook-Air kadai10-i21itikohara % ./mysysmain "ls -l"
+mysystem:
+total 624
+-rw-r--r--  1 ichikoharario  staff     143  7  4 09:45 Makefile
+-rw-r--r--  1 ichikoharario  staff    2795  7  4 09:45 README.md
+-rw-r--r--@ 1 ichikoharario  staff  238232  7  4 09:45 README.pdf
+-rwxr-xr-x  1 ichikoharario  staff   50376  7  9 15:01 mysysmain
+-rw-r--r--  1 ichikoharario  staff     933  7  9 14:58 mysysmain.c
+-rw-r--r--  1 ichikoharario  staff    1517  7  9 15:14 mysystem.c
+-rw-r--r--  1 ichikoharario  staff      90  7  4 09:45 mysystem.h
+-rw-r--r--@ 1 ichikoharario  staff      20  7  4 10:11 x.txt
+retval = 00000000
+system:
+total 624
+-rw-r--r--  1 ichikoharario  staff     143  7  4 09:45 Makefile
+-rw-r--r--  1 ichikoharario  staff    2795  7  4 09:45 README.md
+-rw-r--r--@ 1 ichikoharario  staff  238232  7  4 09:45 README.pdf
+-rwxr-xr-x  1 ichikoharario  staff   50376  7  9 15:01 mysysmain
+-rw-r--r--  1 ichikoharario  staff     933  7  9 14:58 mysysmain.c
+-rw-r--r--  1 ichikoharario  staff    1517  7  9 15:14 mysystem.c
+-rw-r--r--  1 ichikoharario  staff      90  7  4 09:45 mysystem.h
+-rw-r--r--@ 1 ichikoharario  staff      20  7  4 10:11 x.txt
+retval = 00000000
+
+　〜コマンドが存在しない場合〜
+ichikoharario@ichikohararionoMacBook-Air kadai10-i21itikohara % ./mysysmain aaa 
+mysystem:
+sh: aaa: command not found
+retval = 00007f00
+system:
+sh: aaa: command not found
+retval = 00007f00
+
+　〜コマンドを入力しない場合〜
+ichikoharario@ichikohararionoMacBook-Air kadai10-i21itikohara % ./mysysmain    
+使い方 : ./mysysmain コマンド文字列
 
 */
